2080b624c59950d84596c496a8eeb17921b06d43
negbook <negbook@gmail.com>
2022-02-17 01:15:58 +0800

    .

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,8 +14,6 @@ files {
 	'tasksync_once.lua.sourcecode',
 	'tasksync_with_scaleform.lua.sourcecode',
 	'tasksync_with_drawtext.lua.sourcecode',
-	'tasksync_with_drawmenu.lua.sourcecode',
-	'tasksync_with_menu.lua.sourcecode',
 	'tasksync_with_keys.lua.sourcecode',
 }
 client_scripts {
