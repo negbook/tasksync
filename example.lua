a31b65846a224a8ac7b4d613bca324858c2bec4d
negbook <negbook@gmail.com>
2022-03-09 14:23:15 +0800

    update

--- example.lua
+++ example.lua
@@ -17,12 +17,12 @@ Tasksync.KeyContainer.Create("test","test_keys",{
         {"RIGHT","JUST_PRESSED",function(...) print('cb:',...) end , "fuck"},
         
         {"RIGHT","JUST_HOLDED",function(...) print('cb:',...) end , "fuck"},
-        --{"RIGHT","PRESSED",function(...) print('cb:',...) end , "fuck"},
+        {"RIGHT","PRESSED",function(...) print('cb:',...) end , "fuck"},
         {"RIGHT","JUST_RELEASED",function(...) print('cb:',...) end , "fuck"},
         {"DOWN","JUST_RELEASED",function(...) print('cb:',...) end , "fuck"},
         {{"Q","E","W"},"JUST_PRESSED",function(...) print('cb:',...) end , "fuck"},
         {{"Q","E","W"},"JUST_HOLDED",function(...) print('cb:',...) end , "fuck"},
-        --{{"Q","E","W"},"PRESSED",function(...) print('cb:',...) end , "fuck"},
+        {{"Q","E","W"},"PRESSED",function(...) print('cb:',...) end , "fuck"},
         {{"Q","E","W"},"JUST_RELEASED",function(...) print('cb:',...) end , "fuck"},
     }
 })
